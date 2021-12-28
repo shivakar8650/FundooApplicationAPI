b0fce5cb9837d577d7bf1a1d3e7834ab5aa58d2f
shivakar <shivakar@ch.iitr.ac.in>
2021-12-28 10:04:48 +0530

    create 3Tier architecture and add migration and connect database

---
+++ RepositoryLayer/Context/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RepositoryLayer.Enitity;
+
+namespace RepositoryLayer.Context
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> UserTable
+        {
+            get; set;
+        }
+    }
+
+}
---
+++ RepositoryLayer/Enitity/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Enitity
+{
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        
+        [Required(ErrorMessage = "First Name is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email Id is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "EmailId")]
+        public string EmailId { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Password contain six character")]
+        public string Password { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? Createdat { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? Modified { get; set; }
+    }
+}
